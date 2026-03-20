62f0f236f92e4fe0fc1afd1d541473306854a405
Sayali Hatwar <sayali.hatwar4321@gmail.com>
2026-03-20 18:21:22 +0530

    First commit

---
+++ str_loop.py
@@ -0,0 +1,7 @@
+Arc_address = "Gurudev Nagar Main Rd, beside HPCL Gurudeo Petrol Pump, Chota Tajbag, Nandanvan, Nagpur, Maharashtra 440024"
+
+# string is iteration type -- it can be used in looping
+for ch in Arc_address:
+    print(f"ch = {ch}")
+    print("-"*5)
+
